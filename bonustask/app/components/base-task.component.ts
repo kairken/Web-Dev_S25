2b031a77345431041ef68dab2fa6676b1e722a92
Kulshum Kairken <157514879+kairken@users.noreply.github.com>
2025-03-09 17:48:50 +0600

    Delete bonustask directory

--- bonustask/app/components/base-task.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Input } from '@angular/core';
-import { Task } from '../models/task.model';
-
-@Component({
-  template: ''
-})
-export class BaseTaskComponent {
-  @Input() task!: Task;
-}
--- bonustask/app/models/task.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Task {
-  id: number;
-  title: string;
-  description: string;
-  category: string;
-  status: 'pending' | 'completed';
-  deadline: Date;
-}
